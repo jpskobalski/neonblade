9360e9cd6aeb7a2f05b4e5cb98ea119bb3f66194
juanskobalski <jskobalski@oceansoundconsulting.com>
2025-04-20 10:37:51 -0300

    cyberart

---
+++ art/cyberart.js
@@ -0,0 +1,8 @@
+// cyberart.js
+
+document.addEventListener("DOMContentLoaded", () => {
+    const gallery = document.querySelector(".visuals-grid");
+  
+    // optional: lazy loading or lightbox hooks can go here later
+  });
+  
